d338cf1958fca4718052fb93cfd414a5f14b077f
Nathanael Björkgård <nathanael@bjorkgard.se>
2014-05-10 16:01:47 +0200

    Swedish translation

---
+++ src/lang/sv/date.php
@@ -0,0 +1,25 @@
+<?php 
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Date Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the date library. Each line can
+    | have a singular and plural translation separated by a '|'.
+    |
+    */
+
+    'ago'       => ':time sedan',
+    'from now'  => ':time från nu',
+    'year'      => '1 år|:number år',
+    'month'     => '1 månad|:number månader',
+    'week'      => '1 vecka|:number veckor',
+    'day'       => '1 dag|:number dagar',
+    'hour'      => '1 timma|:number timmar',
+    'minute'    => '1 minut|:number minuter',
+    'second'    => '1 sekund|:number sekunder',
+
+);
