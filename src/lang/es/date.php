bb7ea7d33ec5f4067a6ae40a2bd4478d346bba11
Sendoa Portuondo <sendoa@gmail.com>
2014-06-25 16:49:37 +0200

    Added spanish translation

---
+++ src/lang/es/date.php
@@ -0,0 +1,48 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Date Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the date library. Each line can
+    | have a singular and plural translation separated by a '|'.
+    |
+    */
+
+    'ago'       => 'Hace :time',
+    'from now'  => 'Dentro de :time',
+    'after'     => ':time después',
+    'before'    => ':time antes',
+    'year'      => '1 año|:number años',
+    'month'     => '1 mes|:number meses',
+    'week'      => '1 semana|:number semanas',
+    'day'       => '1 día|:number días',
+    'hour'      => '1 hora|:number horas',
+    'minute'    => '1 minuto|:number minutos',
+    'second'    => '1 segundo|:number segundos',
+
+    'january'   => 'Enero',
+    'february'  => 'Febrero',
+    'march'     => 'Marzo',
+    'april'     => 'Abril',
+    'may'       => 'Mayo',
+    'june'      => 'Junio',
+    'july'      => 'Julio',
+    'august'    => 'Agosto',
+    'september' => 'Septiembre',
+    'october'   => 'Octubre',
+    'november'  => 'Noviembre',
+    'december'  => 'Diciembre',
+
+    'monday'    => 'Lunes',
+    'tuesday'   => 'Martes',
+    'wednesday' => 'Miércoles',
+    'thursday'  => 'Jueves',
+    'friday'    => 'Viernes',
+    'saturday'  => 'Sábado',
+    'sunday'    => 'Domingo',
+
+);
