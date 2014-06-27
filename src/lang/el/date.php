51e0ae9e1bd164d5afd9637653a2b6bf5565f974
Dimitris Zavantias <dzavantias@gmail.com>
2014-06-27 06:47:31 +0300

    Added Greek translation

---
+++ src/lang/el/date.php
@@ -0,0 +1,48 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Date Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the date library. Each line can
+    | have a singular and plural translation separated by a '|'.
+    |
+    */
+
+    'ago'       => 'πρίν απο :time',
+    'from now'  => 'σε :time απο τώρα',
+    'after'     => ':time μετά',
+    'before'    => ':time πρίν',
+    'year'      => '1 χρόνος|:number χρόνια',
+    'month'     => '1 μήνας|:number μήνες',
+    'week'      => '1 εβδομάδα|:number εβδομάδες',
+    'day'       => '1 μέρα|:number μέρες',
+    'hour'      => '1 ώρα|:number ώρες',
+    'minute'    => '1 λεπτό|:number λεπτά',
+    'second'    => '1 δευτερόλεπτο|:number δευτερόλεπτα',
+
+    'january'   => 'Ιανουάριος',
+    'february'  => 'Φεβρουάριος',
+    'march'     => 'Μάρτιος',
+    'april'     => 'Απρίλιος',
+    'may'       => 'Μάιος',
+    'june'      => 'Ιούνιος',
+    'july'      => 'Ιούλιος',
+    'august'    => 'Αύγουστος',
+    'september' => 'Σεπτέμβριος',
+    'october'   => 'Οκτώβριος',
+    'november'  => 'Νοέμβριος',
+    'december'  => 'Δεκέμβριος',
+
+    'monday'    => 'Δευτέρα',
+    'tuesday'   => 'Τρίτη',
+    'wednesday' => 'Τετάρτη',
+    'thursday'  => 'Πέμπτη',
+    'friday'    => 'Παρασκευή',
+    'saturday'  => 'Σάββατο',
+    'sunday'    => 'Κυριακή',
+
+);
