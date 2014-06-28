2395d97102fb15b53b2a2f0d85dd7ce777f0722f
mmestrovic <mmestrovic@gmail.com>
2014-06-28 22:16:37 +0200

    Added Croatian translation.

---
+++ src/lang/hr/date.php
@@ -0,0 +1,48 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Date Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the date library. Each line can
+    | have a singular and plural translation separated by a '|'.
+    |
+    */
+
+    'ago'       => 'Prije :time',
+    'from now'  => ':time od sad',
+    'after'     => 'nakon :time',
+    'before'    => 'prije :time',
+    'year'      => '1 godina|:number godine',
+    'month'     => '1 mjesec|:number mjeseci',
+    'week'      => '1 tjedan|:number tjedana',
+    'day'       => '1 dan|:number dana',
+    'hour'      => '1 sat|:number sati',
+    'minute'    => '1 minutu|:number minuta',
+    'second'    => '1 sekundu|:number sekundi',
+
+    'january'   => 'Siječanj',
+    'february'  => 'Veljača',
+    'march'     => 'Ožujak',
+    'april'     => 'Travanj',
+    'may'       => 'Svibanj',
+    'june'      => 'Lipanj',
+    'july'      => 'Srpanj',
+    'august'    => 'Kolovoz',
+    'september' => 'Rujan',
+    'october'   => 'Listopad',
+    'november'  => 'Studeni',
+    'december'  => 'Prosinac',
+
+    'monday'    => 'Ponedjeljak',
+    'tuesday'   => 'Utorak',
+    'wednesday' => 'Srijeda',
+    'thursday'  => 'Četvrtak',
+    'friday'    => 'Petak',
+    'saturday'  => 'Subota',
+    'sunday'    => 'Nedjelja',
+
+);
