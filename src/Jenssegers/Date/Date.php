a6165b294aff43c201e217fb4fb04c70ab9b895b
Jens Segers <segers.jens@gmail.com>
2014-07-27 18:45:27 +0200

    Added diff suffix for german

--- src/Jenssegers/Date/Date.php
+++ src/Jenssegers/Date/Date.php
@@ -112,7 +112,11 @@ class Date extends Carbon {
         // Some languages have different unit translations when used in combination
         // with a specific suffix. Here we will check if there is an optional
         // translation for that specific suffix and use it if it exists.
-        if ($lang->get("date::date.${unit}_${suffix}") != "date::date.${unit}_${suffix}")
+        if ($lang->get("date::date.${unit}_diff") != "date::date.${unit}_diff")
+        {
+            $ago = $lang->choice("date::date.${unit}_diff", $difference);
+        }
+        else if ($lang->get("date::date.${unit}_${suffix}") != "date::date.${unit}_${suffix}")
         {
             $ago = $lang->choice("date::date.${unit}_${suffix}", $difference);
         }
--- src/lang/de/date.php
+++ src/lang/de/date.php
@@ -45,12 +45,12 @@ return array(
     'saturday'  => 'Samstag',
     'sunday'    => 'Sonntag',
 
-    'year_ago'      => '1 Jahr|:count Jahren',
-    'month_ago'     => '1 Monat|:count Monaten',
-    'week_ago'      => '1 Woche|:count Wochen',
-    'day_ago'       => '1 Tag|:count Tagen',
-    'hour_ago'      => '1 Stunde|:count Stunden',
-    'minute_ago'    => '1 Minute|:count Minuten',
-    'second_ago'    => '1 Sekunde|:count Sekunden',
+    'year_diff'      => '1 Jahr|:count Jahren',
+    'month_diff'     => '1 Monat|:count Monaten',
+    'week_diff'      => '1 Woche|:count Wochen',
+    'day_diff'       => '1 Tag|:count Tagen',
+    'hour_diff'      => '1 Stunde|:count Stunden',
+    'minute_diff'    => '1 Minute|:count Minuten',
+    'second_diff'    => '1 Sekunde|:count Sekunden',
 
 );
