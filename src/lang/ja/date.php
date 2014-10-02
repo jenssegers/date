2690915c9db7808646d6f200a3c0355a19f9294c
Kerry Usry <kerry@corebean.com>
2014-10-02 11:48:53 -0700

    Add Japanese localization.

---
+++ src/lang/ja/date.php
@@ -0,0 +1,25 @@
+<?php 
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Date Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the date library. Each line can
+    | have a singular and plural translation separated by a '|'.
+    |
+    */
+
+    'ago'       => '前',
+    'from now'  => '今から',
+    'year'      => '年 | 年',
+    'month'     => '月 | 月',
+    'week'      => '週 | 週',
+    'day'       => '日 | 日',
+    'hour'      => '時間 | 時間',
+    'minute'    => '分 | 分',
+    'second'    => '秒 | 秒',
+
+);
