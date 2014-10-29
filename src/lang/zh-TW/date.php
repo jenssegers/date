fb7a7088e32856e311b1a870c509e019a8e207ae
Isaac <isaactinghong@gmail.com>
2014-10-29 21:35:35 +0800

    Create date.php for zh-TW (Traditional Chinese)

---
+++ src/lang/zh-TW/date.php
@@ -0,0 +1,48 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Date Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the date library. Each line can
+    | have a singular and plural translation separated by a '|'.
+    |
+    */
+
+    'ago'       => ':time前',
+    'from_now'  => '距現在 :time',
+    'after'     => ':time後',
+    'before'    => ':time前',
+    'year'      => '1 年|:count 年',
+    'month'     => '1 月|:count 月',
+    'week'      => '1 周|:count 周',
+    'day'       => '1 天|:count 天',
+    'hour'      => '1 小時|:count 小時',
+    'minute'    => '1 分鐘|:count 分鐘',
+    'second'    => '1 秒|:count 秒',
+
+    'january'   => '一月',
+    'february'  => '二月',
+    'march'     => '三月',
+    'april'     => '四月',
+    'may'       => '五月',
+    'june'      => '六月',
+    'july'      => '七月',
+    'august'    => '八月',
+    'september' => '九月',
+    'october'   => '十月',
+    'november'  => '十一月',
+    'december'  => '十二月',
+
+    'monday'    => '星期一',
+    'tuesday'   => '星期二',
+    'wednesday' => '星期三',
+    'thursday'  => '星期四',
+    'friday'    => '星期五',
+    'saturday'  => '星期六',
+    'sunday'    => '星期日',
+
+);
