6a405d709ee3b33e54b645059c04137e30d5f916
Rati Wannapanop <rati.wannapanop@gmail.com>
2014-12-20 01:30:02 +0000

    add Thai langauage file

---
+++ src/lang/th/date.php
@@ -0,0 +1,48 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Date Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the date library. Each line can
+    | have a singular and plural translation separated by a '|'.
+    |
+    */
+
+    'ago'       => ':time ที่แล้ว',
+    'from_now'  => ':time จากนี้',
+    'after'     => 'หลัง:time',
+    'before'    => 'ก่อน:time',
+    'year'      => '1 ปี|:count ปี',
+    'month'     => '1 เดือน|:count เดือน',
+    'week'      => '1 สัปดาห์|:count สัปดาห์',
+    'day'       => '1 วัน|:count วัน',
+    'hour'      => '1 ชั่วโมง|:count ชั่วโมง',
+    'minute'    => '1 นาที|:count นาที',
+    'second'    => '1 วินาที|:count วินาที',
+
+    'january'   => 'มกราคม',
+    'february'  => 'กุมภาพันธ์',
+    'march'     => 'มีนาคม',
+    'april'     => 'เมษายน',
+    'may'       => 'พฤษภาคม',
+    'june'      => 'มิถุนายน',
+    'july'      => 'กรกฎาคม',
+    'august'    => 'สิงหาคม',
+    'september' => 'กันยายน',
+    'october'   => 'ตุลาคม',
+    'november'  => 'พฤศจิกายน',
+    'december'  => 'ธันวาคม',
+
+    'monday'    => 'จันทร์',
+    'tuesday'   => 'อังคาร',
+    'wednesday' => 'พุธ',
+    'thursday'  => 'พฤหัสบดี',
+    'friday'    => 'ศุกร์',
+    'saturday'  => 'เสาร์',
+    'sunday'    => 'อาทิตย์',
+
+);
