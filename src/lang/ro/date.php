27c55d2d397a4b763407fb79cd9444bf19a7624c
Arthur Kirkosa <arthur.kirkosa@me.com>
2015-01-12 17:25:12 +0200

    ro_RO: Added Romanian language

---
+++ src/lang/ro/date.php
@@ -0,0 +1,48 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Date Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the date library. Each line can
+    | have a singular and plural translation separated by a '|'.
+    |
+    */
+
+    'ago'       => 'acum :time',
+    'from_now'  => ':time de acum',
+    'after'     => 'peste :time',
+    'before'    => 'acum :time',
+    'year'      => 'un an|:count ani',
+    'month'     => 'o luna|:count luni',
+    'week'      => 'o saptamana|:count saptamani',
+    'day'       => 'o zi|:count zile',
+    'hour'      => 'o ora|:count ore',
+    'minute'    => 'un minut|:count minute|:count minute',
+    'second'    => 'o secunda|:count secunde',
+
+    'january'   => 'ianuarie',
+    'february'  => 'februarie',
+    'march'     => 'martie',
+    'april'     => 'aprilie',
+    'may'       => 'mai',
+    'june'      => 'iunie',
+    'july'      => 'iulie',
+    'august'    => 'august',
+    'september' => 'septembrie',
+    'october'   => 'octobrie',
+    'november'  => 'novembrie',
+    'december'  => 'decembrie',
+
+    'monday'    => 'luni',
+    'tuesday'   => 'marti',
+    'wednesday' => 'miercuri',
+    'thursday'  => 'joi',
+    'friday'    => 'vineri',
+    'saturday'  => 'sambata',
+    'sunday'    => 'duminica',
+
+);
