793d60c6347b957e946625bd62a5e7ddb91d8671
Jens Segers <segers.jens@gmail.com>
2015-01-29 23:18:37 +0100

    Remove the package registration for L5

--- src/Jenssegers/Date/DateServiceProvider.php
+++ src/Jenssegers/Date/DateServiceProvider.php
@@ -18,8 +18,6 @@ class DateServiceProvider extends ServiceProvider {
      */
     public function boot()
     {
-        $this->package('jenssegers/date');
-
         // Use the Laravel translator.
         Date::setTranslator($this->app['translator']);
     }
