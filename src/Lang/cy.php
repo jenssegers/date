c5b95a8b4add08a2dadceb450b948ac4c68b76c3
Mei Gwilym <mei.gwilym@gmail.com>
2016-04-05 16:15:44 +0100

    translation file for Welsh (Cymraeg) language

---
+++ src/Lang/cy.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Date Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the date library. Each line can
+    | have a singular and plural translation separated by a '|'.
+    |
+    */
+
+    'ago'       => ':time yn ôl',
+    'from_now'  => 'mewn :time',
+    'after'     => ':time ar ôl',
+    'before'    => ':time cyn',
+    'year'      => '1 blwyddyn|2 flynedd|:count mlynedd',
+    'month'     => '1 mis|2 fis|:count mis',
+    'week'      => ':count wythnos',
+    'day'       => ':count diwrnod',
+    'hour'      => ':count awr',
+    'minute'    => '1 munud|2 funud|:count munud',
+    'second'    => ':count eiliad',
+
+    'january'   => 'Ionawr',
+    'february'  => 'Chwefror',
+    'march'     => 'Mawrth',
+    'april'     => 'Ebrill',
+    'may'       => 'Mai',
+    'june'      => 'Mehefin',
+    'july'      => 'Gorffenaf',
+    'august'    => 'Awst',
+    'september' => 'Medi',
+    'october'   => 'Hydref',
+    'november'  => 'Tachwedd',
+    'december'  => 'Rhagfyr',
+
+    'monday'    => 'Dydd Llun',
+    'tuesday'   => 'Dydd Mawrth',
+    'wednesday' => 'Dydd Mercher',
+    'thursday'  => 'Dydd Iau',
+    'friday'    => 'Dydd Gwener',
+    'saturday'  => 'Dydd Sadwrn',
+    'sunday'    => 'Dydd Sul',
+
+];
