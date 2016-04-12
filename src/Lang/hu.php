ca51c7eb8ebacd4da7a1cf20ce7738412f4fd480
balping <balping314@gmail.com>
2016-04-12 18:09:32 +0200

    Update hu.php

--- src/Lang/hu.php
+++ src/Lang/hu.php
@@ -1,6 +1,6 @@
 <?php
 
-return array(
+return [
 
     /*
     |--------------------------------------------------------------------------
@@ -69,4 +69,4 @@ return array(
     'saturday'  => 'szombat',
     'sunday'    => 'vasárnap',
 
-);
+];
