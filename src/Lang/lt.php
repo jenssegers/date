66678ea7657574df50a4e0652107d58e13ae84da
Simonas Dimavičius <asd@asd.lt>
2016-05-03 14:10:32 +0300

    Lithuanian language support
    Title self-explanatory.

---
+++ src/Lang/lt.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Date Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the date library. Each line can
+    | have a singular and plural translation separated by a '|'.
+    |
+    */
+
+    'ago'       => 'prieš :time',
+    'from_now'  => ':time nuo dabar',
+    'after'     => 'po :time',
+    'before'    => 'prieš :time',
+    'year'      => '1 metai|:count metai',
+    'month'     => '1 mėnesis|:count mėnesiai',
+    'week'      => '1 savaitė|:count savaitės',
+    'day'       => '1 diena|:count dienos',
+    'hour'      => '1 valanda|:count valandos',
+    'minute'    => '1 minutė|:count minutės',
+    'second'    => '1 sekundė|:count sekundės',
+
+    'january'   => 'Sausis',
+    'february'  => 'Vasaris',
+    'march'     => 'Kovas',
+    'april'     => 'Balandis',
+    'may'       => 'Gegužė',
+    'june'      => 'Birželis',
+    'july'      => 'Liepa',
+    'august'    => 'Rugpjūtis',
+    'september' => 'Rugsėjis',
+    'october'   => 'Spalis',
+    'november'  => 'Lapkritis',
+    'december'  => 'Gruodis',
+
+    'monday'    => 'Pirmadienis',
+    'tuesday'   => 'Antradienis',
+    'wednesday' => 'Trečiadienis',
+    'thursday'  => 'Ketvirtadienis',
+    'friday'    => 'Penktadienis',
+    'saturday'  => 'Šeštadienis',
+    'sunday'    => 'Sekmadienis',
+
+];
