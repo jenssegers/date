21a745a11be87c5fd55f3a58022ac6fdf7493ebe
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2016-08-31 04:52:01 +0600

    Added Bangla Language

---
+++ src/Lang/bd.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Date Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the date library. Each line can
+    | have a singular and plural translation separated by a '|'.
+    |
+    */
+
+    'ago'       => ':time পূর্বে',
+    'from_now'  => ':time এখন থেকে',
+    'after'     => ':time পরে',
+    'before'    => ':time আগে',
+    'year'      => '1 বছর',
+    'month'     => '1 মাস',
+    'week'      => '1 সপ্তাহ',
+    'day'       => '1 দিন',
+    'hour'      => '1 ঘন্টা',
+    'minute'    => '1 মিনিট',
+    'second'    => '1 সেকেন্ড',
+
+    'january'   => 'জানুয়ারী',
+    'february'  => 'ফেব্রুয়ারি',
+    'march'     => 'মার্চ',
+    'april'     => 'এপ্রিল',
+    'may'       => 'মে',
+    'june'      => 'জুন',
+    'july'      => 'জুলাই',
+    'august'    => 'আগস্ট',
+    'september' => 'সেপ্টেম্বর',
+    'october'   => 'অক্টোবর',
+    'november'  => 'নভেম্বর',
+    'december'  => 'ডিসেম্বর',
+
+    'monday'    => 'সোমবার',
+    'tuesday'   => 'মঙ্গলবার',
+    'wednesday' => 'বুধবার',
+    'thursday'  => 'বৃহস্পতিবার',
+    'friday'    => 'শুক্রবার',
+    'saturday'  => 'শনিবার',
+    'sunday'    => 'রবিবার',
+
+];
