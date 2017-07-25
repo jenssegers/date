bc37c8d3b0a55da6f3eb92385df5c2b5559388a5
Samson Endale <4sam21@gmail.com>
2017-07-25 16:34:29 +0300

    Added amharic local

---
+++ src/Lang/am.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Date Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the date library. Each line can
+    | have a singular and plural translation separated by a '|'.
+    |
+    */
+
+    'ago'       => 'ከዛሬ :time በፊት',
+    'from_now'  => 'ከዛሬ :time በኋላ',
+    'after'     => 'ከ :time በኋላ',
+    'before'    => 'ከ :time በፊት',
+    'year'      => '1 አመት|:count አመታት',
+    'month'     => '1 ወር|:count ወራት',
+    'week'      => '1 ሳምንት|:count ሳምንታት',
+    'day'       => '1 ቀን|:count ቀናት',
+    'hour'      => '1 ሰአት|:count ሰዓታት',
+    'minute'    => '1 ደቂቃ|:count ደቂቃዎች',
+    'second'    => ':count ሰከንድ',
+
+    'january'   => 'ጥር',
+    'february'  => 'የካቲት',
+    'march'     => 'መጋቢት',
+    'april'     => 'ሚያዝያ',
+    'may'       => 'ግንቦት',
+    'june'      => 'ሰኔ',
+    'july'      => 'ሐምሌ',
+    'august'    => 'ነሐሴ',
+    'september' => 'መስከረም',
+    'october'   => 'ጥቅምት',
+    'november'  => 'ኅዳር',
+    'december'  => 'ታኅሣሥ',
+
+    'monday'    => 'ሰኞ',
+    'tuesday'   => 'ማክሰኞ',
+    'wednesday' => 'ረቡዕ',
+    'thursday'  => 'ሓሙስ',
+    'friday'    => 'ዓርብ',
+    'saturday'  => 'ቅዳሜ',
+    'sunday'    => 'እሑድ',
+
+];
