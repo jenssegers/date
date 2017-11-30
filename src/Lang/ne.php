4a1aa003f61133d2b72306cdcc2a218bbfe9e264
Nootan Ghimire <nootan.ghimire@gmail.com>
2017-11-30 18:20:41 +0700

    Add Nepali (ne) Locale
    Notes:
      - Month names are transliterated. Wikipedia was used as source.
      - Similarly, second and minute are transliterated.

---
+++ src/Lang/ne.php
@@ -0,0 +1,56 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Date Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the date library. Each line can
+    | have a singular and plural translation separated by a '|'.
+    |
+    */
+
+    'ago'       => ':time पहिले',
+    'from_now'  => ':time देखी',
+    'after'     => ':time पछि',
+    'before'    => ':time अघि',
+    'year'      => ':count वर्ष',
+    'month'     => ':count महिना',
+    'week'      => ':count हप्ता',
+    'day'       => ':count दिन',
+    'hour'      => ':count घण्टा',
+    'minute'    => ':count मिनेट',
+    'second'    => ':count सेकेण्ड',
+
+    'january'   => 'जेनवरी',
+    'february'  => 'फेब्रुअरी',
+    'march'     => 'मार्च',
+    'april'     => 'अप्रिल',
+    'may'       => 'मे',
+    'june'      => 'जून',
+    'july'      => 'जूलाई',
+    'august'    => 'अगस्ट',
+    'september' => 'सेप्टेम्बर',
+    'october'   => 'अक्टोबर',
+    'november'  => 'नोभेम्बर',
+    'december'  => 'डिसेम्बर',
+
+    'sunday'    => 'आइतवार',
+    'monday'    => 'सोमवार',
+    'tuesday'   => 'मङ्गलवार',
+    'wednesday' => 'बुधवार',
+    'thursday'  => 'बिहिवार',
+    'friday'    => 'शुक्रवार',
+    'saturday'  => 'शनिवार',
+
+    'sun'       => 'आइत',
+    'mon'       => 'सोम',
+    'tue'       => 'मङ्गल',
+    'wed'       => 'बुध',
+    'thu'       => 'बिहि',
+    'fri'       => 'शुक्र',
+    'sat'       => 'शनि',
+
+];
