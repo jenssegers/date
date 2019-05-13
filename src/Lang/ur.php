efd0fbd6bb6192169731a47a90a86c64edf84b1f
nassim nasibullah <spinzar.inc@gmail.com>
2019-05-13 20:29:25 +0900

    Add files via upload

---
+++ src/Lang/ur.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Date Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the date library. Each line can
+    | have a singular and plural translation separated by a '|'.
+    |
+    */
+
+    'ago' => ':time پہلے',
+    'from_now' => 'اب سے :time',
+    'after' => ':time کے بعد',
+    'before' => ':time پہلے',
+    'year' => ':count سال',
+    'month' => ':count ماہ',
+    'week' => ':count ہفتوں',
+    'day' => ':count دن',
+    'hour' => ':count گھنٹہ',
+    'minute' => ':count منٹ',
+    'second' => ':count سیکنڈ',
+
+    'january' => 'جنوری',
+    'february' => 'فروری',
+    'march' => 'مارچ',
+    'april' => 'اپریل',
+    'may' => 'مئی',
+    'june' => 'جون',
+    'july' => 'جولائی',
+    'august' => 'اگست',
+    'september' => 'ستمبر',
+    'october' => 'اکتوبر',
+    'november' => 'نومبر',
+    'december' => 'دسمبر',
+
+    'monday' => 'پیر',
+    'tuesday' => 'منگل',
+    'wednesday' => 'بدھ',
+    'thursday' => 'جمعرات',
+    'friday' => 'جمعہ',
+    'saturday' => 'ہفتہ',
+    'sunday' => 'اتوار',
+
+    'mon' => 'پیر',
+    'tue' => 'منگل',
+    'wed' => 'بدھ',
+    'thu' => 'جمعرات',
+    'fri' => 'جمعہ',
+    'sat' => 'ہفتہ',
+    'sun' => 'اتوار',
+];
