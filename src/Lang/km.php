af9d79c97bc02aa719646b22ae99a22b0f08ce15
HEL Mab <mabhelitc@gmail.com>
2019-07-04 17:40:24 +0700

    add Cambodia-khmer language.

---
+++ src/Lang/km.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Date Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the date library. Each line can
+    | have a singular and plural translation separated by a '|'.
+    |
+    */
+
+    'ago' => ':time រួចមកហើយ',
+    'from_now' => ':time ចាប់ពីឥឡូវនេះ',
+    'after' => ':time ក្រោយ',
+    'before' => ':time មុន',
+    'year' => ':count ឆ្នាំ',
+    'month' => ':count ខែ',
+    'week' => ':count សប្តាហ៍',
+    'day' => ':count ថ្ងៃ',
+    'hour' => ':count ម៉ោង',
+    'minute' => ':count នាទី',
+    'second' => ':count វិនាទី',
+
+    'january' => 'មករា',
+    'february' => 'កុម្ភៈ',
+    'march' => 'មីនា',
+    'april' => 'មេសា',
+    'may' => 'ឧសភា',
+    'june' => 'មិថុនា',
+    'july' => 'កក្តដា',
+    'august' => 'សីហា',
+    'september' => 'កញ្ញា',
+    'october' => 'តុលា',
+    'november' => 'វិច្ឆិកា',
+    'december' => 'ធ្ធូ',
+
+    'monday' => 'ច័ន្ទ',
+    'tuesday' => 'អង្គារ',
+    'wednesday' => 'ពុធ',
+    'thursday' => 'ព្រហស្បតិ៍',
+    'friday' => 'សុក្រ',
+    'saturday' => 'សៅរ៍',
+    'sunday' => 'អាទិត្យ',
+
+    'mon' => 'ច.',
+    'tue' => 'អ.',
+    'wed' => 'ព.',
+    'thu' => 'ព្រ.',
+    'fri' => 'សុ.',
+    'sat' => 'ស.',
+    'sun' => 'អា.',
+];
