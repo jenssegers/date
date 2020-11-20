ac187f6a2ea600e06b3b2b9741e68d44468552da
Giovanni Lopez <gmlo89@hotmail.com>
2020-11-20 12:36:44 -0700

    Trait for attributes of models

---
+++ src/Traits/DatesTranslator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Jenssegers\Date\Traits;
+
+use Jenssegers\Date\Date;
+
+trait DatesTranslator {
+    
+    /**
+     * Get the attribute created_at like a instance of Date
+     *
+     * @param [type] $created_at
+     * @return void
+     */
+    public function getCreatedAtAttribute($created_at)
+    {
+        return new Date($created_at);
+    }
+
+    /**
+     * Get the attribute updated_at like a instance of Date
+     *
+     * @param [type] $updated_at
+     * @return void
+     */
+    public function getUpdatedAtAttribute($updated_at)
+    {
+        return new Date($updated_at);
+    }
+
+    /**
+     * Get the attribute deleted_at like a instance of Date
+     *
+     * @param [type] $deleted_at
+     * @return void
+     */
+    public function getDeletedAtAttribute($deleted_at)
+    {
+        return new Date($deleted_at);
+    }
+}
